0ccf3d4b9c021ff1a891dd04741b8277f3e1c8fc
Phloser <phloser@icloud.com>
2021-01-22 16:29:08 +0300

    Task1

--- home_work/Program.cs
+++ home_work/Program.cs
@@ -6,7 +6,35 @@ namespace home_work
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Task1
+            /* Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку.
+            а) используя склеивание;
+            б) используя форматированный вывод;
+            в) *используя вывод со знаком $.*/
+
+            // Блок запроса данных
+            Console.WriteLine("Ваше Имя?");
+              string name1 = Console.ReadLine();
+             Console.WriteLine("Фамилия?");
+               string name2 = Console.ReadLine();
+             Console.WriteLine("Сколько Вам лет?");
+              int age = int.Parse(Console.ReadLine());
+              Console.WriteLine("Каков Ваш рост?");
+              int higth = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько Вы весите?");
+              int weigth = int.Parse(Console.ReadLine());
+
+            // Склеевание
+            Console.WriteLine("Здравствуйте, " + name1 + " " + name2 + "! Возраст "+ age + " лет" + ", рост " + higth + "см, вес " + weigth + "кг");
+
+            // Форматированный вывод
+            Console.WriteLine(string.Format("Здравствуйте, {0} {1}! Возраст {2} лет, рост {3}см, вес {4}кг", name1, name2, age, higth, weigth));
+
+            // Вывод со знаком $
+            Console.WriteLine(string.Format($"Здравствуйте, {name1} {name2}! Возраст {age} лет, рост {higth}см, вес {weigth}кг"));
+
+            Console.ReadKey();
+            #endregion
         }
     }
 }
